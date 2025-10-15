bc3f220fd1b96de9d83a900e9005ac43998bc2b9
Dhani Dwi Putra <dhanidwiputra371@gmail.com>
2025-10-15 12:31:51 +0700

    menyelesaikan landingpage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(function() {
     "use strict";
+    //------- Lightbox  js --------//  
+    $('.img-gal').magnificPopup({
+      type: 'image',
+      gallery: {
+          enabled: true
+      }
+    });
+
 
     //------- testimonial carousel --------//  
     if($('.owl-carousel').length > 0){
@@ -100,7 +108,27 @@ $(function() {
 
   //------- Active Nice Select --------//
   $('select').niceSelect();
+  
 
+  //--------sroom click-----------//
+  const imgs = document.querySelectorAll('.img-select a');
+  const imgBtns = [...imgs];
+  let imgId = 1;
 
+  imgBtns.forEach((imgItem) => {
+      imgItem.addEventListener('click', (event) => {
+          event.preventDefault();
+          imgId = imgItem.dataset.id;
+          slideImage();
+      });
+  });
+
+  function slideImage(){
+      const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+
+      document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+  }
+
+  window.addEventListener('resize', slideImage);
 
 });
