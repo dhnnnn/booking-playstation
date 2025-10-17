213d41b383f20a4fa32ac115b3ed539cfd524481
Dhani Dwi Putra <dhanidwiputra371@gmail.com>
2025-10-17 13:22:46 +0700

    Memperbaiki bug click

--- js/main.js
+++ js/main.js
@@ -116,19 +116,21 @@ $(function() {
   let imgId = 1;
 
   imgBtns.forEach((imgItem) => {
-      imgItem.addEventListener('click', (event) => {
-          event.preventDefault();
-          imgId = imgItem.dataset.id;
-          slideImage();
-      });
+    imgItem.addEventListener('click', (event) => {
+      event.preventDefault();
+      imgId = imgItem.dataset.id;
+      slideImage();
+    });
   });
 
-  function slideImage(){
-    const displayWidth = document.querySelector('.img-showcase img:first-child');
+  function slideImage() {
+    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
 
-    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+    document.querySelector('.img-showcase').style.transform = 
+      `translateX(${-(imgId - 1) * displayWidth}px)`;
   }
 
   window.addEventListener('resize', slideImage);
+
 
 });
